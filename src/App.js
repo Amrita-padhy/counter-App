f9a99e5af7fdacde693d397659f82cd024ec352a
amrita <priyaamrita@5gmail.com>
2022-11-11 17:50:33 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+  const handleSubtract = () => {
+    setCount(count - 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  let color = "#ffffff";
+  if (count >= 0) {
+    color = "yellow";
+  }
+  if (count < 0) {
+    color = "red";
+  } else {
+    color = "white";
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="counter-sec --flex-center">
+        <div className="container counter --card --center-all">
+          <h1 className="--text-light">React Counter App</h1>
+          <p
+            className="count --my2 --text-md --text-light --fw-bold"
+            style={{ color: color }}
+          >
+            {count}
+          </p>
+          <div className="buttons">
+            <button className="--btn --btn-danger" onClick={handleSubtract}>
+              - Subtract
+            </button>
+            <button className="--btn " onClick={handleReset}>
+              Reset
+            </button>
+            <button className="--btn --btn-success" onClick={handleAdd}>
+              + Add
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
